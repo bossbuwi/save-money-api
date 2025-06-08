98cbe06930ed4365400ee423cfafe8fb1802cfd1
Paradox Devs <paradoxhorizondevs@gmail.com>
2025-06-08 20:47:00 +0800

    Initial commit.

---
+++ src/main/java/com/paradox/savemoney/SaveMoneyApiApplication.java
@@ -0,0 +1,13 @@
+package com.paradox.savemoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaveMoneyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaveMoneyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paradox/savemoney/SaveMoneyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paradox.savemoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaveMoneyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
