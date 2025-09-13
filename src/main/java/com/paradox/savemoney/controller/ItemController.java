dc71f754d2ee87bad0e512f0e0693cb741d11a8b
bossbuwi <paradoxhorizondevs@gmail.com>
2025-09-14 07:53:10 +0800

    Remove unused local database

--- src/main/java/com/paradox/savemoney/controller/ItemController.java
+++ src/main/java/com/paradox/savemoney/controller/ItemController.java
@@ -3,10 +3,8 @@ package com.paradox.savemoney.controller;
 import com.paradox.savemoney.api.supabase.model.CreateItemRequest;
 import com.paradox.savemoney.api.supabase.model.UpdateItemRequest;
 import com.paradox.savemoney.api.supabase.service.SupabaseApiService;
-import com.paradox.savemoney.service.ItemService;
 import com.paradox.savemoney.util.HttpUtils;
 import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
@@ -15,16 +13,9 @@ import reactor.core.publisher.Mono;
 @RequestMapping("/api/items")
 public class ItemController {
     private final SupabaseApiService supabaseApiService;
-    private final ItemService itemService;
 
-    public ItemController(SupabaseApiService supabaseApiService, ItemService itemService) {
+    public ItemController(SupabaseApiService supabaseApiService) {
         this.supabaseApiService = supabaseApiService;
-        this.itemService = itemService;
-    }
-
-    @GetMapping("/test")
-    public ResponseEntity<String> test() {
-        return new ResponseEntity<>(itemService.test(), HttpStatus.OK);
     }
 
     @GetMapping(value = "/index")
--- src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO items (_id, amount, description) VALUES (1, 1000, 'Item 1');
-INSERT INTO items (_id, amount, description) VALUES (2, 100, 'Item 2');
-INSERT INTO items (_id, amount, description) VALUES (3, 10, 'Item 3');
-INSERT INTO items (_id, amount, description) VALUES (4, 1, 'Item 4');
-INSERT INTO items (_id, amount, description) VALUES (5, 123, 'Item 5');
-ALTER TABLE items ALTER COLUMN _id RESTART WITH 6;
